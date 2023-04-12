c1394ab6ce59386e59e1c2b85a8c11bec20ee082
smshaown <smshaown65@gmail.com>
2023-04-12 22:46:15 +0600

    not found done

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='my-container mt-10 text-center'>
-            <h1 className='text-3xl font bold text-red-600 mb-4'>404</h1>
+            <h1 className='text-3xl font bold mb-4'>404</h1>
             <h3 className='text- 2xl font-semibold text-orange-800'>Not Found This Page</h3>
+
+
+            
         </div>
     );
 };
