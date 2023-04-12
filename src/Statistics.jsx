001b3f50b0ca9f4af8fcab2f76c6631de5e1cbea
smshaown <smshaown65@gmail.com>
2023-04-12 22:41:29 +0600

    header done

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,42 +1,56 @@
-import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
+  const assignmentMarks = [
+    { id: 1, name: "A-1", marks: 59 },
+    { id: 2, name: "A-2", marks: 59 },
+    { id: 3, name: "A-3", marks: 60 },
+    { id: 4, name: "A-4", marks: 59 },
+    { id: 5, name: "A-5", marks: 60 },
+    { id: 6, name: "A-6", marks: 60 },
+    { id: 7, name: "A-7", marks: 60 },
+  ];
 
-    const assignmentMarks = [
-        { id: 1, name: "A-1", marks: 59 },
-        { id: 2, name: "A-2", marks: 59},
-        { id: 3, name: "A-3", marks: 60 },
-        { id: 4, name: "A-4", marks: 59 },
-        { id: 5, name: "A-5", marks: 60 },
-        { id: 6, name: "A-6", marks: 60 },
-        { id: 7, name: "A-7", marks: 60 }
-    ];
-
-    let total = 0;
-  for(let i = 0; i < assignmentMarks.length; i++) {
+  let total = 0;
+  for (let i = 0; i < assignmentMarks.length; i++) {
     total += assignmentMarks[i].marks;
   }
   const average = total / assignmentMarks.length;
   console.log("Average value:", average);
 
+  return (
+    <div className="my-container mt-8">
+      <h1 className="mb-4 text-2xl md:text-4xl font-bold text-regalBlack text-center ">
+        Assignment Analytics:        
+      </h1>
+    
+      
+      <AreaChart
+        className="mx-auto"
+        width={700}
+        height={400}
+        data={assignmentMarks}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
 
-    return (
-        <div className='my-container mt-8'>
-            <h1 className='mb-4 text-2xl md:text-4xl font-bold text-regalBlack text-center'>Assignment Analytics</h1>
-            <AreaChart
-                className='mx-auto'
-                width={700}
-                height={400}
-                data={assignmentMarks}
-            >
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Area type="monotone" dataKey="marks" stroke="#8884d8" fill="#8884d8" />
-            </AreaChart>
-        </div>
-    );
+      <h2 className="mb-4 text-2xl md:text-4xl font-bold text-regalBlack text-center ">
+       Average Mark: {average.toFixed(2)} 
+      </h2>
+    </div>
+  );
 };
 
 export default Statistics;
